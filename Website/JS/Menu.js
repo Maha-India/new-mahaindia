cdd8e4e222607dc7e53439ae94dfc599257bdda7
Maha-India <131330837+Maha-India@users.noreply.github.com>
2024-11-10 14:49:51 +0530

    Update Menu.js

--- Website/JS/Menu.js
+++ Website/JS/Menu.js
@@ -1,21 +1,47 @@
-$(document).ready(function(){
-    if(window.innerWidth>800){$("#toggle").remove();}else{
+$(document).ready(function() {
+    if (window.innerWidth > 800) {
+        $("#toggle").remove();
+    } else {
         $("nav").hide();
         $("#page").hide();
-        $("#toggle").click(function(){
-            
+
+        $("#toggle").click(function() {
             $("body").toggle();
-            $("nav").toggle();
-            $("nav").toggleClass('navigate navigate-on');
-            $(".navigate-on").css({"background-color":"black","display":"block","align":"center","width":"100vw","left":"0","margin":"0 0 0 0","top":"60px","z-index":"1","position":"absolute"});
-            $("#page").toggle();
-            $("#page").toggleClass('page page-on');
-            $(".page-on").css({"background-color":"black","display":"block","align":"center","width":"100vw","left":"0","padding":"0 0 0 0","z-index":"1","position":"absolute"});
-            $("main").toggle ();
+            $("nav").toggle().toggleClass('navigate navigate-on');
+            $("#page").toggle().toggleClass('page page-on');
+            $("main").toggle();
             $("footer").toggle();
             $("#header-image-menu").toggle();
-            $("#toggle").show();
-          });
 
+            // Apply inline styles for .navigate-on
+            if ($("nav").hasClass('navigate-on')) {
+                $("nav").css({
+                    "background-color": "black",
+                    "display": "block",
+                    "text-align": "center",  // Updated from 'align' to 'text-align'
+                    "width": "100vw",
+                    "left": "0",
+                    "margin": "0",
+                    "top": "60px",
+                    "z-index": "1",
+                    "position": "absolute"
+                });
+            }
+
+            // Apply inline styles for .page-on
+            if ($("#page").hasClass('page-on')) {
+                $("#page").css({
+                    "background-color": "black",
+                    "display": "block",
+                    "text-align": "center",  // Updated from 'align' to 'text-align'
+                    "width": "100vw",
+                    "left": "0",
+                    "padding": "0",
+                    "z-index": "1",
+                    "position": "absolute"
+                });
+            }
+        });
     }
 });
+            
