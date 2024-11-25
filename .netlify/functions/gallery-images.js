effab19866848081c49441d325cb3b7b29dc41b0
Maha-India <131330837+Maha-India@users.noreply.github.com>
2024-11-25 20:36:07 +0530

    Update and rename gallery-images.js to .netlify/functions/gallery-images.js

---
+++ .netlify/functions/gallery-images.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
+// Netlify function to fetch image and video metadata
+exports.handler = async function() {
+  // Adjust the path to point to the images folder in the root/Abheek/images directory
+  const mediaDir = path.join(__dirname, '../../Abheek/images');
+  
+  try {
+    // Read the media directory
+    const files = fs.readdirSync(mediaDir);
+    
+    // Filter images and videos based on extensions (case insensitive)
+    const media = files
+      .filter(file => /\.(jpg|jpeg|png|mp4|movie)$/i.test(file.toLowerCase()))  // Convert file names to lowercase
+      .map(file => {
+        const stats = fs.statSync(path.join(mediaDir, file));
+        return {
+          filename: file,
+          dateCreated: stats.mtime,  // Use modification time as creation date
+          altText: file.replace(/[-_]/g, ' ').replace(/\.[a-z]+$/i, '')  // Create alt text by removing extension and replacing hyphens
+        };
+      });
+
+    // Sort media by dateCreated (newest first)
+    media.sort((a, b) => new Date(b.dateCreated) - new Date(a.dateCreated));
+
+    // Return the sorted media metadata
+    return {
+      statusCode: 200,
+      body: JSON.stringify(media)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Error reading media directory' })
+    };
+  }
+};
